02de8eb4287e74411207f63a940796f10ff56055
Shawn.J <shawnking07@outlook.com>
2020-11-11 00:42:22 +0800

    update

--- README.md
+++ README.md
@@ -6,7 +6,14 @@
 
 ## Task 3
 
-task3.sh: adapt for segnet
+**requirements**: 
+bash
+```
+pip install split-folders 
+pip install detectron2 -f https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.7/index.html (depends on pytorch version)
+```
+
+task3.sh: copy to new folder and construct new structure for Detectron2
 
 preprocessed dataset: [Google Drive](https://drive.google.com/file/d/1zs5QEUkS0nzTlYlfO-jMJZxQTLtPL9oB/view?usp=sharing)
 
--- gen_train_data.py
+++ gen_train_data.py
@@ -4,37 +4,10 @@ import os
 import json
 from pprint import pprint
 
-<<<<<<< HEAD
-path_base = './task3_dataset/annotation/'
-filenames = [path_base + f for f in os.listdir(path_base)]
-images = [cv2.imread(f, cv2.IMREAD_GRAYSCALE) for f in filenames]
-borders = []
-
-for i in images:
-    colors = np.unique(i)[1:]
-    contours=[cv2.findContours(np.where(i == c, 255, 0).astype(np.uint8), mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)[0] for c in colors]
-    
-    for cnts in range(len(contours)):
-        print('color: '+ str(cnts))
-        for k in contours[cnts]:
-            k=k.reshape(-1,2)
-            poly = [(x + 0.5, y + 0.5) for x, y in zip(k[:,0], k[:,1])]
-            print(len(poly))
-
-
-# check the borders
-# try:
-#     os.mkdir('borders')
-# except Exception:
-#     pass
-# for img_idx, b in enumerate(borders):
-#     for obj_idx, o in enumerate(b):
-#         cv2.imwrite('borders\{}_{}.png'.format(img_idx, obj_idx), o)
-=======
 path_origin_base = './task3_dataset/annotation/'
 
 
-def gen_coco_json(path_base: str):
+def gen_via_json(path_base: str):
     """ 
     Generate JSON file for training, 
     this function will tries to find the contour of each leaf in every images
@@ -78,9 +51,8 @@ def gen_coco_json(path_base: str):
             y, x = y.tolist(), x.tolist()
             this_region["all_points_x"], this_region["all_points_y"] = x, y
 
-    with open('coco.json', 'w') as outfile:
+    with open('via.json', 'w') as outfile:
         outfile.write(json.dumps(roots))
 
 
-gen_coco_json(path_origin_base)
->>>>>>> c6f0bc9d0e3979eece0eb3174b24e4d7ac9ef367
+gen_via_json(path_origin_base)
